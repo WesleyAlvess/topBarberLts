032e09c39a1cb224704dc927c048807579baca18
WesleyAlvess <walvespereira96@gmail.com>
2025-03-02 10:05:24 -0300

    created updateHorarios

--- src/controllers/horario.controller.js
+++ src/controllers/horario.controller.js
@@ -63,9 +63,28 @@ export const getHorarios = async (req, res) => {
 // Listando Horários por Colaborador
 export const getHorariosPorColaborador = async (req, res) => {
   try {
-    const { salaoId } = req.params;
+    const { salaoId, colaboradorId } = req.params; // Pega o ID do salão e do colaborador da URL
+
+    // Verifica se os IDs são válidos
+    if(!salaoId || !colaboradorId) {
+      return res.status(400).json({ message: "ID do salão ou do colaborador não fornecido!" });
+    }
+
+    // Busca os horários do colaborador no salão
+    const horario = await Horario.find({
+      salao: salaoId,
+      colaborador: colaboradorId,
+    })
+
+    // Verifica se o horário foi encontrado
+    if(!horario) {
+      return res.status(404).json({ message: "Horário não encontrado!" });
+    }
+
+    return res.status(200).json(horario); // Retorna o horário 
+
   } catch (err) {
-    return res.status(500).json({ mensagem: "Erro ao criar horário", err });
+    return res.status(500).json({ mensagem: "Erro ao buscar horários do profissional", err });
   }
 }
 
@@ -73,9 +92,45 @@ export const getHorariosPorColaborador = async (req, res) => {
 // Atualizar Horário
 export const updateHorario = async (req, res) => {
   try {
-    const { salaoId } = req.params;
+    const { salaoId, id } = req.params; // Pegando SalaoId da url da requisição
+    const colaboradorUsuario = req.usuario.id //Pega o colaborador/usuario do objeto da requisição
+    const { dias, inicio, fim } = req.body; // Pegando os dados do corpo da requisição
+
+    // Verifica se os campos estão vazios
+    if ( !dias || !inicio || !fim) {
+      return res.status(400).json({ message: "Preencha todos os campos!" });
+    }
+
+    // Busca o horário no banco de dados
+    const horarioAtualizado = await Horario.findByIdAndUpdate(
+      // Condição de busca
+      {
+      _id: id,
+      salao: salaoId,
+      colaborador: colaboradorUsuario,
+      },
+      // Novos valores
+      {
+        dias,
+        inicio,
+        fim,
+      },
+      // Retorna o documento atualizado
+      {
+        new: true,
+      }
+  )
+
+    // Verifica se o horário foi encontrado
+    if(!horarioAtualizado) {
+      return res.status(404).json({ message: "Horário não encontrado!" });
+    }
+
+    // Resposta da request
+    return res.status(200).json(horarioAtualizado);
+
   } catch (err) {
-    return res.status(500).json({ mensagem: "Erro ao criar horário", err });
+    return res.status(500).json({ mensagem: "Erro ao atualizar horário", err });
   }
 }
 
--- src/routes/servico.route.js
+++ src/routes/servico.route.js
@@ -22,14 +22,10 @@ import { verificaDonoRecurso } from "../middlewares/verificaDonoRecurso.middlewa
 // 🔓 Rotas públicas
 router.get("/:salaoId", getServicos); // Listar serviços de um salão
 router.get("/:salaoId/:id", getServicoById); // Listar um serviço específico
-// router.get("/:salaoId/horarios", getHorarios); // Listar horários disponíveis do salão
 
-// // 🔒 Rotas protegidas (apenas donos do salão podem gerenciar serviços e horários)
+// 🔒 Rotas protegidas (apenas donos do salão podem gerenciar serviços e horários)
 router.post("/:salaoId", verificarToken, verificaDonoRecurso(Salao), createServico); // Criar serviço
 router.patch("/:salaoId/:id", verificarToken, verificaDonoRecurso(Salao), updateServico); // Atualizar serviço
 router.delete("/:salaoId/:id", verificarToken, verificaDonoRecurso(Salao), deleteServico); // Deletar serviço
-
-// router.post("/:salaoId/horarios", verificarToken, verificaDonoRecurso(Salao), createHorario); // Criar horário
-// router.delete("/:salaoId/horarios/:id", verificarToken, verificaDonoRecurso(Salao), deleteHorario); // Remover horário
 
 export default router;
